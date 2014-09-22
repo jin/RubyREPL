b423e5f073a469d9a1eec7f2f1516f5463b64472
jin <jin@crypt.sg>
2014-09-23 00:55:44 +0800

    working, but not sandboxed

--- app.rb
+++ app.rb
@@ -1,25 +1,44 @@
 require 'twitter'
 require 'yaml'
+require 'shikashi'
+
+include Shikashi
 
 user_config = YAML::load_file "user_config.yml"
+@s = Sandbox.new
+@priv = Privileges.new
+@priv.allow_method :print
 
-client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = user_config.consumer_key
-  config.consumer_secret     = user_config.consumer_secret 
-  config.access_token        = user_config.access_token 
-  config.access_token_secret = user_config.access_token_secret 
+@stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = user_config["consumer_key"]
+  config.consumer_secret     = user_config["consumer_secret"]
+  config.access_token        = user_config["access_token"]
+  config.access_token_secret = user_config["access_token_secret"]
 end
 
-topics = ["ruby"]
-client.filter(:track => topics.join(",")) do |object|
-  puts object.text if object.is_a?(Twitter::Tweet)
+@rest_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = user_config["consumer_key"]
+  config.consumer_secret     = user_config["consumer_secret"]
+  config.access_token        = user_config["access_token"]
+  config.access_token_secret = user_config["access_token_secret"]
 end
 
-client.user do |object|
-  case object
-  when Twitter::Streaming::Event
-    puts "It's a tweet!"
-  when Twitter::Streaming::StallWarning
-    warn "Falling behind!"
+def evaluate_tweet(tweet)
+  begin
+    eval_output = eval tweet.text.split(' ')[1..-1].join(' ') 
+     
+    # eval_output = @s.run(priv, tweet.text.split(' ')[1..-1].join(' '))
+    @rest_client.update("@#{tweet.user.name} #{eval_output}")
+  rescue Exception => exc
+    @rest_client.update("@#{tweet.user.name} I'm sorry, I don't understand your input!")
   end
 end
+
+topics = ["repltweet"]
+@stream_client.filter(:track => topics.join(",")) do |object|
+  p object
+  if object.is_a?(Twitter::Tweet)
+    evaluate_tweet(object)
+  end
+end
+
