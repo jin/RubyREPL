b14e47530a57671f61634a5b0a4b6779ada04b97
jin <jin@crypt.sg>
2014-10-14 01:18:10 +0800

    Forgot to require logger

--- lib/rubyrepl.rb
+++ lib/rubyrepl.rb
@@ -1,4 +1,5 @@
 require_relative 'engine'
-$logger = Logger.new('rubyrepl.log', 20, 'daily')
+require 'logger'
 
+$logger = Logger.new('rubyrepl.log', 20, 'daily')
 RubyREPL::Engine.new.start_stream
