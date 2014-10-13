b09bb4a650afca1e091a5ab32099fef923222b2e
jin <jin@crypt.sg>
2014-10-14 01:03:52 +0800

    Refactored and restructured engine.rb, and added logging

--- lib/engine.rb
+++ lib/engine.rb
@@ -5,46 +5,62 @@ require_relative 'config'
 module RubyREPL
   class Engine
 
+    attr_reader :stream_client, :rest_client
+    attr_reader :sandbox, :priv
+
+    CONFIG_STRINGS = ["consumer_key",
+                      "consumer_secret",
+                      "access_token",
+                      "access_token_secret"]
+
     def initialize
-      user_config = RubyREPL::Configuration.config
-      config_strings = ["consumer_key", 
-                        "consumer_secret", 
-                        "access_token", 
-                        "access_token_secret"]
+      user_config = Configuration.config
+      @stream_client = Twitter::Streaming::Client.new { |config| apply_config(config, user_config) }
+      @rest_client   = Twitter::REST::Client.new      { |config| apply_config(config, user_config) }
 
-      @stream_client = Twitter::Streaming::Client.new do |config|
-        config_strings.each { |str| config.send("#{str}=", user_config[str]) }
-      end
+      init_sandbox_and_privileges
+    end
 
-      @rest_client = Twitter::REST::Client.new do |config|
-        config_strings.each { |str| config.send("#{str}=", user_config[str]) }
+    def apply_config(client_config, user_config)
+      CONFIG_STRINGS.each { |str| client_config.send("#{str}=", user_config[str]) }
+    end
+
+    def init_sandbox_and_privileges
+      @sandbox = Shikashi::Sandbox.new
+      @priv = Shikashi::Privileges.new
+
+      [:"+", :"-", :"*", :"/", :"%", :"**"].each { |m| priv.allow_method m }
+    end
+
+    def start_stream
+      @stream_client.user(:with => "user") do |object|
+        evaluate_tweet(object) if object.is_a?(Twitter::Tweet) && object.user.screen_name != "RubyREPL"
       end
     end
 
     def evaluate_tweet(tweet)
       begin
-        sandbox = Shikashi::Sandbox.new
-        priv = Shikashi::Privileges.new
-        priv.allow_method :"+"
-        priv.allow_method :"*"
-        eval_output = sandbox.run(tweet.text.split(' ')[1..-1].join(' '), priv) 
-        # eval_output = eval tweet.text.split(' ')[1..-1].join(' ')
-        @rest_client.update("@#{tweet.user.screen_name} #{eval_output}",
-                           :in_reply_to_status => tweet)
+        eval_output = sandboxed_eval(get_code_from_tweet(tweet))
+        reply_to_tweet(tweet, eval_output)
       rescue Exception => exc
-        @rest_client.update(
-          "@#{tweet.user.screen_name} I'm sorry, I don't understand your input!", 
-          :in_reply_to_status => tweet)
-        p exc
+        $logger.error "#{exc}: #{tweet.text}"
+        reply_to_tweet(tweet, "@#{tweet.user.screen_name} I'm sorry, I don't understand your input!")
       end
     end
 
-    def start_stream
-      @stream_client.user(:with => "user") do |object|
-        p object
-        evaluate_tweet(object) if object.is_a?(Twitter::Tweet) && 
-                                  object.user.screen_name != "RubyREPL"
-      end
+    def sandboxed_eval(code)
+      @sandbox.run(code, @priv)
+    end
+
+    def reply_to_tweet(source, content)
+      $logger.info "Reply to #{source.user.screen_name}: #{content}"
+      @rest_client.update("@#{source.user.screen_name} #{content}", :in_reply_to_status => source)
+    end
+
+    # utility
+
+    def get_code_from_tweet(tweet)
+      tweet.text.split(' ')[1..-1].join(' ')
     end
 
   end
